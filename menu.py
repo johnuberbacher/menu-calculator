0f3df7eb3b3e3490bacbf43b9c943ce559eed5b6
John Uberbacher <johnuberbacher@gmail.com>
2017-07-03 22:08:29 -0400

    Function and Menu.txt updates
    Threw most of the app into a function that can keep running.
    Streamlined the inputreading. Converting the values over to print
    correctly feels a bit sloppy. There might be a better way to parse
    everything. Also added some sample dishes to menu.txt

--- menu.py
+++ menu.py
@@ -1,21 +1,27 @@
 import os
+	
+# Configure and load menuArray items into memory
+menuArray = []
+with open('menu.txt') as inputfile:
+	for line in inputfile:
+		menuArray.append(line.strip().split(','))
 
-menu = []
-textFile = open("menu.txt")
-lines = textFile.readlines()
-for line in lines:
-	menu.append(line.split(" "))
-
-data = input("Enter a string of numbers: ")
-try:
-	data = int(data)
-except ValueError:
-	print("\nSorry, enter an integer")
-else:
-	if data > 0:
-		for letter in data:
-			print (str(letter))
+def start():
+	# Prompt user for orders
+	# TODO exit command(s)
+	userInput = input("Enter a string of numbers: ")
+	try:
+		userInput = int(userInput)
+	except ValueError:
+		print("\nSorry, enter an integer")
 	else:
-		print("\nSorry, that won't work, enter an integer")
-			
-
+		if userInput >= 0:
+			print ("\nOrder Received: #" + str(userInput))
+			for letter in str(userInput):
+				print ("Your item: " + str(*menuArray[int(letter)]))	
+		else:
+			print("\nSorry, that won't work, enter an integer")
+		
+# Keep the program running, 
+while True:
+	start()
